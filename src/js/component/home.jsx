ac9487e2425fc5214955f84b2ed6390f1bff693b
iNelan <clpayala@gmail.com>
2022-05-31 20:25:12 +0000

    version2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,25 @@ const Home = () => {
 	const [color, setcolor] = useState("red");
 
 	return (
-		<div className="Traffic">
-			<div
-				onClick={() => setcolor("red")}
-				className={
-					"red light" + (color === "red" ? " shine" : "")
-				}></div>
-			<div
-				onClick={() => setcolor("yellow")}
-				className={
-					"yellow light" + (color === "yellow" ? " shine" : "")
-				}></div>
-			<div
-				onClick={() => setcolor("green")}
-				className={
-					"green light" + (color === "green" ? " shine" : "")
-				}></div>
+		<div>
+			<div className="Traffic">
+				<div
+					onClick={() => setcolor("red")}
+					className={
+						"red light" + (color === "red" ? " shine" : "")
+					}></div>
+				<div
+					onClick={() => setcolor("yellow")}
+					className={
+						"yellow light" + (color === "yellow" ? " shine" : "")
+					}></div>
+				<div
+					onClick={() => setcolor("green")}
+					className={
+						"green light" + (color === "green" ? " shine" : "")
+					}></div>
+			</div>
+			<button type="button" class="btn btn-outline-warning">Warning</button>
 		</div>
 	);
 };
